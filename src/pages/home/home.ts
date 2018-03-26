bfff79826e86f53e5f1158ecd4b9b3865f50b2f9
poteii <poteii.pasel@icloud.com>
2018-03-26 16:13:22 +0700

    by poteii
    Day1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  loginResult;
+
   constructor(public navCtrl: NavController) {
+
+  }
+
+  //method
+  signIn(_username, _password) {
+    // alert('OK ' + _username + ', ' + _password);
+    //ส่งไป web api เพื่อตรวจสอบ
+    if (_password == '1234') {
+      //pass!
+      this.loginResult = "ผ่าน!";
+    } else {
+      //fail!
+      this.loginResult = "โปรดตรวจสอบข้อมูลและลองอีกครั้ง";
+    }
 
   }
 
